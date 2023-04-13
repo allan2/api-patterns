709ee5075d17a481c2c891a126c69ba2554ee6c9
Allan <al@ayz.ai>
2023-04-13 01:45:20 -0400

    Change comments

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ mod fluent;
 mod simple;
 
 fn main() {
-    // mutable input struct is simpler
+    // mutable input struct is a simpler implementation
     let client = SimpleClient::new();
     let mut input = SayHelloInput::default();
     input.first_name = "Foo".to_owned();
     input.last_name = "Bar".to_owned();
     client.say_hello(input);
 
-    // fluent builder allows for function chaining
+    // fluent builder has pretty function chaining
     let client = FluentBuilderClient::new();
     client.say_hello().first_name("Foo").last_name("Bar").send();
 
-    // alternative with fluent builder
+    // fluent builder again
     let mut builder = client.say_hello();
     builder.first_name("Foo");
     builder.last_name("Bar");
